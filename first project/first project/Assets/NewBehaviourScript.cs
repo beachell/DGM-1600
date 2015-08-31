2ae2f8bef8fd3cb9dc76bde2decc6ae465bbac3a
Brad Beachell <10529740@CS518-20.ad.uvu.edu>
2015-08-31 14:42:24 -0600

    My first unity project in DGM 1600

---
+++ first project/first project/Assets/NewBehaviourScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	int myInt = 22;
+	// Use this for initialization
+	void Start () {
+		
+
+		Debug.Log (myInt);
+	}
+
+	int MultiplyByTwo (int number)
+	{
+		int ret;
+		ret = number * 2;
+		return ret;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//changes the color of the cube when r key is pressed.
+		if (Input.GetKeyDown (KeyCode.R)) 
+		{
+			gameObject.GetComponent<Renderer>().material.color = Color.red;
+			myInt = MultiplyByTwo(myInt);
+		}
+
+
+	}
+}
