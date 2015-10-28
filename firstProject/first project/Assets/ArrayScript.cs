5dc7022025a34a37a7019a130ddb7965bf26eb19
Brad Beachell <10529740@CS518-25.ad.uvu.edu>
2015-10-28 16:51:55 -0600

    Arrays

--- firstProject/first project/Assets/ArrayScript.cs
+++ firstProject/first project/Assets/ArrayScript.cs
@@ -3,10 +3,12 @@ using System.Collections;
 
 public class ArrayScript : MonoBehaviour {
 
+	public string[] arrayName = new string[8];
+	public string[] arrayName2 = {"yay", "i'm a second line"};
 	// Use this for initialization
 	void Start () {
 			
-		string[] arrayName = new string[8];
+
 		
 		arrayName[0]= "count down"; 
 		arrayName[1]= "7"; 
@@ -15,14 +17,17 @@ public class ArrayScript : MonoBehaviour {
 		arrayName[4]="4" ; 
 		arrayName[5]="3" ; 
 		arrayName[6]= "Boom"; 
-		arrayName[7]="guess the fuze was short" ; 
+		arrayName[7]="guess the fuze was shorter than we thought." ; 
 
 
 		for(int i=0;i<arrayName.Length; i++)
 		{
 
 			Debug.Log(arrayName[i]);
+
+			
 		}
+		Debug.Log(arrayName2[0]);
 	}
 	
 	// Update is called once per frame
