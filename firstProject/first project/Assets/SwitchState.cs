fd9c7ada0080c3d5d396c9b1c78125434ed350ed
Brad Beachell <10529740@CS518-25.ad.uvu.edu>
2015-11-04 14:55:23 -0700

    Switch Statements

---
+++ firstProject/first project/Assets/SwitchState.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwitchState : MonoBehaviour {
+
+	public int gameDifficulty = 4;
+
+	void ChooseYourPain ()
+	{
+		switch (gameDifficulty)
+		{
+		case 2:
+			print ("why is this so hard");
+			break;
+		case 1:
+			print ("this is normal");
+			break;
+
+		default:
+				print ("this is easy as pie");
+			break;
+
+		}
+	}
+	void Start()
+	{
+	}
+	void Update()
+	{
+		ChooseYourPain();
+	}
+}
+
