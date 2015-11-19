6bb0a1a12e0e6ea16734495063cd5dd9d9b16e52
Brad Beachell <10529740@CS518-25.ad.uvu.edu>
2015-11-18 17:21:06 -0700

    movement.

---
+++ firstProject/Final projetct 1600/Assets/DuckMovement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DuckMovement : MonoBehaviour {
+
+	public float speed;
+	public Vector3 direction;
+
+	void Start()
+	{
+		RandomDirection ();
+	}
+	void FixedUpdate()
+	{
+		transform.position = transform.position + (direction * speed);
+
+	}
+
+	public void RandomDirection()
+	{
+		direction = new Vector3 (Random.Range (-1, 1), Random.Range (-.2f, 1f), 0);
+	}
+
+	public void DirectionChanger(Vector3 _dir)
+	{
+		direction = new Vector3(direction.x * _dir.x, direction.y * _dir.y, 0);
+	}
+
+}
