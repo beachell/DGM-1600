0fad6b148505afad424c1a7a2b58a08eee525f7c
Brad Beachell <10529740@CS518-25.ad.uvu.edu>
2015-11-30 15:58:28 -0700

    added the captured metriod sprite, still needs to be made to appear in scene.

--- firstProject/Final projetct 1600/Assets/NewBehaviourScript.cs
+++ firstProject/Final projetct 1600/Assets/NewBehaviourScript.cs
@@ -13,12 +13,27 @@ public class NewBehaviourScript : MonoBehaviour {
 	
 	}*/
 
-
+	public int killCount = 0;
+	public GameObject captured_duck;
+	 
+		public void Spawn_captured_Duck()
+	{
+		Debug.Log("spawn caputered duck");
+		Instantiate(captured_duck,transform.position, Quaternion.identity);
+		
+	}
 	void OnTriggerEnter(Collider hit)
 	{
+
 		if (hit.tag == "killZone") 
 		{
+			int totalKills = killCount;
+
 			Destroy(this.gameObject);
+			totalKills ++;
+			Debug.Log (totalKills);
+
+
 
 		}
 	}
