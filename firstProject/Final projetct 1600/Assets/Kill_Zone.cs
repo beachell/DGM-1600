efae9d94bdbaf17dab58d337fd453b10e05a1679
Brad Beachell <10529740@CS518-25.ad.uvu.edu>
2015-12-07 16:23:04 -0700

    12/7 progress

---
+++ firstProject/Final projetct 1600/Assets/Kill_Zone.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Kill_Zone : MonoBehaviour {
+/*
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+*/
+
+public int killCount = 0;
+public GameObject captured_duck;
+
+public void Spawn_captured_Duck()
+{
+	Debug.Log("spawn caputered duck");
+	Instantiate(captured_duck,transform.position, Quaternion.identity);
+	
+}
+void OnTriggerEnter(Collider hit)
+{
+	
+	if (hit.tag == "killZone") 
+	{
+		int totalKills = killCount;
+		
+		Destroy(this.gameObject);
+		//totalKills ++;
+		//Debug.Log (totalKills);
+		
+		//Spawn_captured_Duck;
+		
+		
+		
+	}
+}
+}
