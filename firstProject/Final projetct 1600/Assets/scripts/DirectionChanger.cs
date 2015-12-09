77d3eed487516cbdf887810a878e33ed5535f8c3
Brad Beachell <10529740@CS518-25.ad.uvu.edu>
2015-12-09 15:19:32 -0700

    added the destroy script and score system.

---
+++ firstProject/Final projetct 1600/Assets/scripts/DirectionChanger.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DirectionChanger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public enum Changer{ Horizontal, Vertical};
+	public Changer changer; 
+
+	void OnCollisionEnter (Collision hit)
+	{
+
+		if (hit.transform.tag == "duck") 
+		{
+			DuckMovement movement = hit.gameObject.GetComponent<DuckMovement>();
+
+			if(changer == Changer.Horizontal)
+			{
+				movement.DirectionChanger(new Vector3 (-1, 1, 0));
+			}
+		
+
+			else if (changer == Changer.Vertical) 
+			{
+				movement.DirectionChanger(new Vector3(1, -1, 0));
+			}
+
+		}
+	}
+
+}
---
+++ firstProject/Final projetct 1600/Assets/scripts/Firing_script.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Firing_script : MonoBehaviour {
+
+
+	public float Time_delay = 1f;
+
+
+	public int Score = 0;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void OnMouseDown () 
+	{
+		Debug.Log (Score);
+		//Rigidbody.useGravity = true ;
+		Destroy (gameObject, Time_delay);
+
+		Score = Score+10;
+
+
+	}
+}
